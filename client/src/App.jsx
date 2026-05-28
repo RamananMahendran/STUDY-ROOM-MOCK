0f7eca4fae569f4544ba4ad38143b8c3b964d7ba
Mayur-k-s <gksmh20@gmail.com>
2026-05-28 14:08:08 +0530

    added login page,signup page,forget-password page and dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -6,6 +10,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/home" element={<Dashboard />} />
       </Routes>
     </Router>
   );
