3d53c59842976704e98f56328c6b8f62573f4859
Coder-Win <sarveshnarayanan4@gmail.com>
2026-06-02 16:08:02 +0100

    cleaned codebase

--- server/dist/src/modules/codeExecution/codeExecutionRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getLanguages, runCode } from './codeExecutionController.js';
-const router = express.Router();
-router.get('/languages', getLanguages);
-router.post('/run', runCode);
-export default router;
--- server/dist/src/modules/rooms/roomRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { getRooms, createRoom } from './roomController.js';
-const router = Router();
-router.get('/', getRooms);
-router.post('/', createRoom);
-export default router;
